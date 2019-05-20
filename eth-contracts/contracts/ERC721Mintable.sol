216a523ace2742a1253ed1e59a3c3a445b32328a
Thomas Weibel <thom@ikhono.net>
2019-05-20 12:07:21 +0200

    Implement Pausable contract

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -44,12 +44,43 @@ contract Ownable {
   }
 }
 
-//  TODO's: Create a Pausable contract that inherits from the Ownable contract
-//  1) create a private '_paused' variable of type bool
-//  2) create a public setter using the inherited onlyOwner modifier 
-//  3) create an internal constructor that sets the _paused variable to false
-//  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
-//  5) create a Paused & Unpaused event that emits the address that triggered the event
+// In real projects, I suggest using 'openzeppelin-solidity/contracts/lifecycle/Pausable.sol' instead of re-implementing it!
+// Create a Pausable contract that inherits from the Ownable contract
+contract Pausable is Ownable {
+  // 1) create a private '_paused' variable of type bool
+  bool private _paused;
+
+  // 2) create a public setter using the inherited onlyOwner modifier
+  function pause() public onlyOwner whenNotPaused {
+    _paused = true;
+    emit Paused(msg.sender);
+  }
+
+  function unpause() public onlyOwner paused {
+    _paused = false;
+    emit Unpaused(msg.sender);
+  }
+
+  // 3) create an internal constructor that sets the _paused variable to false
+  constructor () internal {
+    _paused = false;
+  }
+
+  // 4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
+  modifier whenNotPaused() {
+    require(!_paused, "contract is currently paused");
+    _;
+  }
+
+  modifier paused() {
+    require(_paused, "contract is currently not paused");
+    _;
+  }
+
+  // 5) create a Paused & Unpaused event that emits the address that triggered the event
+  event Paused(address account);
+  event Unpaused(address account);
+}
 
 contract ERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
@@ -87,7 +118,7 @@ contract ERC165 {
     }
 }
 
-contract ERC721 is /*Pausable,*/ ERC165 { // TODO
+contract ERC721 is Pausable, ERC165 {
 
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
 
