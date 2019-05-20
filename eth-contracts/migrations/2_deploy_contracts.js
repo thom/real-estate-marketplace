a9d4e6d73e55499f41c9418782211eb4a958c151
Thomas Weibel <thom@ikhono.net>
2019-05-20 21:10:13 +0200

    Update deployment script to support SolnSquareVerifier

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,15 @@
 // migrating the appropriate contracts
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+var NipaHutERC721Token = artifacts.require("./NipaHutERC721Token.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = async function(deployer) {
+  await deployer.deploy(NipaHutERC721Token);
+  const nipaHutERC721Token = await NipaHutERC721Token.deployed();
+
+  await deployer.deploy(SquareVerifier);
+  const verifierContract = await SquareVerifier.deployed();
+
+  await deployer.deploy(SolnSquareVerifier, verifierContract.address);
+  const solnSquareVerifier = await SolnSquareVerifier.deployed();
 };
